0c1c9c5342b099827ac437d230ee742fb9da9bb0
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2024-04-26 10:48:04 +0530

    Update Assignment1.cpp

--- HPC/Assignment1.cpp
+++ HPC/Assignment1.cpp
@@ -1,88 +1,166 @@
 #include <iostream>
+#include <omp.h>
+
+// #include <bits/stdc++.h>
+#include <ctime>
+#include <iomanip>
+
 #include <vector>
 #include <queue>
-#include <omp.h>
+#include <unordered_set>
 
 using namespace std;
 
-class Graph {
-    int V; // Number of vertices
-    vector<vector<int>> adj; // Adjacency list
+void BFS(vector<vector<int>>& graph, int start)
+{
+    int n=graph.size();
+    vector<bool> visited(n,false);
+    queue<int> q;
 
-public:
-    Graph(int V) : V(V), adj(V) {}
+    visited[start]=true;
+    q.push(start);
 
-    // Function to add an edge between vertices u and v
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u); // Assuming undirected graph
-    }
+    while(!q.empty())
+    {
+        int current=q.front();
+        q.pop();
+        cout<<current<<" ";
 
-    // Breadth First Search
-    void BFS(int start) {
-        vector<bool> visited(V, false);
-        queue<int> q;
-
-        visited[start] = true;
-        q.push(start);
-
-        while (!q.empty()) {
-            int current = q.front();
-            q.pop();
-            cout << current << " ";
-
-            #pragma omp parallel for
-            for (int i = 0; i < adj[current].size(); ++i) {
-                int next = adj[current][i];
-                if (!visited[next]) {
-                    visited[next] = true;
-                    q.push(next);
-                }
+        for(int i=0; i<graph[current].size(); i++)
+        {
+            if(visited[graph[current][i]]==false)
+            {
+                visited[graph[current][i]]=true;
+                q.push(graph[current][i]);
             }
         }
+
     }
+}
 
-    // Depth First Search
-    void DFSUtil(int v, vector<bool>& visited) {
-        visited[v] = true;
-        cout << v << " ";
+void parallelBFS(vector<vector<int>>& graph, int start)
+{
+        int n=graph.size();
+        vector<bool> visited(n,false);
+        queue<int> q;
 
-        #pragma omp parallel for
-        for (int i = 0; i < adj[v].size(); ++i) {
-            int next = adj[v][i];
-            if (!visited[next])
-                DFSUtil(next, visited);
+        visited[start]=true;
+        q.push(start);
+
+    while(!q.empty())
+    {
+        #pragma omp parallel
+        {
+            int current=q.front();
+            q.pop();
+            cout<<current<<" ";
+            #pragma omp for
+            for(int i=0; i<graph[current].size(); i++)
+            {
+                #pragma omp critical
+                if(visited[graph[current][i]]==false)
+                {
+                    visited[graph[current][i]]=true;
+                    q.push(graph[current][i]);
+                }
+            }
+
         }
     }
+}
 
-    void DFS(int start) {
-        vector<bool> visited(V, false);
-        DFSUtil(start, visited);
+void DFSutil(vector<vector<int>>& graph, int current, vector<bool>& visited)
+{
+    visited[current]=true;
+    cout<<current<<" ";
+
+    for(int i=0; i<graph[current].size(); i++)
+    {
+        int adjacent=graph[current][i];
+        if(!visited[adjacent])
+        {
+            DFSutil(graph, adjacent, visited);
+        }
     }
-};
+}
 
+void DFS(vector<vector<int>>& graph, int start)
+{
+    int n= graph.size();
+    vector<bool> visited(n,false);
 
-int main() {
-    Graph g(6); // Creating a graph with 6 vertices
-
-    // Adding edges to the graph
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 5);
-
-
-    cout << "BFS traversal starting from vertex 0: ";
-    g.BFS(0);
-    cout << endl;
-
-    cout << "DFS traversal starting from vertex 0: ";
-    g.DFS(0);
-    cout << endl;
-
-    return 0;
+    DFSutil(graph, start, visited);
 }
 
 
-// g++ -fopenmp graph.cpp -o graph
+void parallelDFSutil(vector<vector<int>>& graph, int current, vector<bool>& visited)
+{
+    visited[current]=true;
+    cout<<current<<" ";
+
+    #pragma omp parallel for
+    for(int i=0; i<graph[current].size(); i++)
+    {
+        int adjacent=graph[current][i];
+        if(!visited[adjacent])
+        {
+            DFSutil(graph, adjacent, visited);
+        }
+    }
+}
+
+void parallelDFS(vector<vector<int>>& graph, int start)
+{
+    int n= graph.size();
+    vector<bool> visited(n,false);
+
+    parallelDFSutil(graph, start, visited);
+}
+
+int main()
+{
+    vector<vector<int>> graph={{1,2},{0,3,4},{0,5},{1},{1},{2}};
+
+    clock_t start, end;
+    double total1, total2;
+
+    start=clock();
+    cout<<"sequential BFS output : ";
+    BFS(graph,0);
+    end=clock();
+
+    total1=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nSequential time : "<<fixed<<setprecision(10)<<total1;
+
+    start=clock();
+    cout<<"\nparallel BFS output : ";
+    parallelBFS(graph,0);
+    end=clock();
+
+    total2=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nParallel time : "<<fixed<<setprecision(10)<<total2;
+
+    cout<<"\nSpeedup : "<<total1/total2;
+
+
+
+    start=clock();
+    cout<<"\n\nsequential DFS output : ";
+    DFS(graph,0);
+    end=clock();
+
+    total1=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nSequential time : "<<fixed<<setprecision(10)<<total1;
+
+    start=clock();
+    cout<<"\nparallel DFS output : ";
+    parallelBFS(graph,0);
+    end=clock();
+
+    total2=double(end-start)/double(CLOCKS_PER_SEC);
+    cout<<"\nParallel time : "<<fixed<<setprecision(10)<<total2;
+
+    cout<<"\nSpeedup : "<<total1/total2;
+
+    return 0;
+}
