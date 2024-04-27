da2676b662275eb60fc386daea4307c14549656a
Hemant Maske <89325376+Hmaske88@users.noreply.github.com>
2024-04-27 09:45:52 +0530

    Update Assignment3.cpp

--- HPC/Assignment3.cpp
+++ HPC/Assignment3.cpp
@@ -1,44 +1,53 @@
-#include<iostream>
-#include<omp.h>
-
+#include <iostream>
+#include <omp.h>
 using namespace std;
-int minval(int arr[], int n){
-  int minval = arr[0];
-  #pragma omp parallel for reduction(min : minval)
-    for(int i = 0; i < n; i++){
-      if(arr[i] < minval) minval = arr[i];
+
+int minVal(int arr[], int n)
+{
+    int minval=arr[0]
+    #pragma omp parallel for reduction(min : minval)
+    ;for(int i=0; i<n; i++)
+    {
+        if(arr[i]<minval) minval=arr[i];
     }
-  return minval;
+    return minval;
 }
 
-int maxval(int arr[], int n){
-  int maxval = arr[0];
-  #pragma omp parallel for reduction(max : maxval)
-    for(int i = 0; i < n; i++){
-      if(arr[i] > maxval) maxval = arr[i];
+int maxVal(int arr[], int n)
+{
+    int maxval=arr[0]
+    #pragma omp parallel for reduction(max : maxval) 
+    ;for(int i=0; i<n; i++)
+    {
+        if(arr[i]>maxval) maxval=arr[i];
     }
-  return maxval;
+    return maxval;
 }
 
-int sum(int arr[], int n){
-  int sum = 0;
-  #pragma omp parallel for reduction(+ : sum)
-    for(int i = 0; i < n; i++){
-      sum += arr[i];
+int sum(int arr[], int n)
+{
+    int sum=0;
+    #pragma omp parallel for reduction(+ : sum) 
+    ;for(int i=0; i<n; i++)
+    {
+        sum+=arr[i];
     }
-  return sum;
+    return sum;
 }
 
-int average(int arr[], int n){
-  return (double)sum(arr, n) / n;
+int average(int arr[],int n)
+{
+    return double(sum(arr,n))/n;
 }
 
-int main(){
-  int n = 5;
-  int arr[] = {1,2,3,4,5};
-  cout << "The minimum value is: " << minval(arr, n) << '\n';
-  cout << "The maximum value is: " << maxval(arr, n) << '\n';
-  cout << "The summation is: " << sum(arr, n) << '\n';
-  cout << "The average is: " << average(arr, n) << '\n';
-  return 0;
+int main()
+{
+    int n=5;
+    int arr[]={1,2,3,4,5};
+    cout<<"Minimum Value : "<<minVal(arr,n)<<"\n";
+    cout<<"Maximum Value : "<<maxVal(arr,n)<<"\n";
+    cout<<"Sum : "<<sum(arr,n)<<"\n";
+    cout<<"Average : "<<average(arr,n)<<"\n";
+
+    return 0;
 }
